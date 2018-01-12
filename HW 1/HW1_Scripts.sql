6dfdf97ccceda60a6a29c5681fb4a29ba3ce197a
Thomas Noelcke <noelcket@oregonstate.edu>
2018-01-12 14:19:59 -0800

    Added Answers for C and D

--- HW 1/HW1_Scripts.sql
+++ HW 1/HW1_Scripts.sql
@@ -10,16 +10,42 @@ SELECT did, dname
   IN (SELECT did
         FROM works
         WHERE works.pct_time > 99);
- --b.
+ -- Question b.
  
- SELECT ename, age FROM emp where emp.eid IN (SELECT eid FROM
-(SELECT eid 
-  FROM works as w
-  WHERE w.did = (SELECT did FROM dept AS d WHERE dname = 'Software')) AS es
-  WHERE eid IN (SELECT eid
-  FROM works as w
-  WHERE w.did = (SELECT did FROM dept AS d WHERE dname = 'Hardware')));
+ SELECT ename, age FROM emp WHERE emp.eid IN 
+	(SELECT eid FROM
+		(SELECT eid 
+			FROM works as w
+				WHERE w.did = 
+					(SELECT did 
+					 FROM dept AS d 
+					 WHERE dname = 'Software')) AS es
+				WHERE eid IN 
+					(SELECT eid
+					 FROM works as w
+					 WHERE w.did = 
+					 (SELECT did 
+					 FROM dept AS d 
+					 WHERE dname = 'Hardware')));
  
     
   
  
+--Question C
+
+SELECT es.ename FROM (SELECT emp.ename, emp.salary, works.did
+  FROM emp, works
+  WHERE emp.eid = works.eid) AS es
+  WHERE es.salary < (SELECT budget from dept WHERE dept.did = es.did);
+
+  
+ -- Question D
+SELECT managerid FROM dept
+	GROUP BY dept.managerid
+	HAVING sum(budget) > 5000000 
+
+	
+-- Question E
+
+
+
