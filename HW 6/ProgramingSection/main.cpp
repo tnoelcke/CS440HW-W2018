c4db57fc02f586c92c6d51119d69a38d6cd34f4f
Thomas Noelcke <noelcket@oregonstate.edu>
2018-02-24 11:26:59 -0800

    Lunch break

--- HW 6/ProgramingSection/main.cpp
+++ HW 6/ProgramingSection/main.cpp
@@ -1,6 +1,7 @@
 
 //author: Thomas Noelcke
-//
+//Class: CS 440 Winter 2018
+//Assignment: HW6
 //
 //Pourpose: The pourpose of this program is to preform a sorted merge join on the two csv files in this
 //Repo. This will be done using merge sort join. However the files are already in sorted order so we will
@@ -30,7 +31,9 @@
 #include<fstream>
 #include<stdlib.h>
 #include<algorithm>
+#include<sys/stat.h>
 using namespace std;
+
 
 //struct definitions
 //sruct constant that will store the length of a character.
@@ -68,6 +71,12 @@ typedef struct empDepartment
     long int managerId;    
 } empDepartment;
 
+typedef struct logEntry{
+    int empPos;
+    int depPos;
+    int joinPos;
+} logEntry;
+
 bool compareEmp(employee* lhs, employee* rhs){ return lhs->eid < rhs->eid; }
 bool compareDep(department* lhs, department* rhs) {return lhs->managerId < rhs->managerId;}
 
@@ -85,9 +94,16 @@ void writeEmpDepartment(FILE* file, empDepartment* join);
 void foundAMatch(FILE* output, FILE* depFile, department* dep, employee* emp);
 void freeEmp(employee* emp);
 void freeDep(department* dep);
-void join(FILE* output, FILE* depFile, FILE* empFile);
+void join(FILE* output, FILE* depFile, FILE* empFile, FILE* logFile);
 void sortDep();
-
+void writeLog(FILE* logFile, logEntry* entry);
+logEntry* readLog(FILE* Log);
+void displayLog(logEntry* toDisplay);
+void recover(FILE* logFile, FILE* depFile, FILE* empFile, FILE* output);
+void outPutLog(FILE* logFile, long int dep, long int emp, long int output);
+logEntry* getLastEntry(FILE* logFile);
+bool fileExits(const char* fname);
+void testCrash();
 
 //Constants
 const int NUM_BLOCKS = 22;
@@ -95,23 +111,48 @@ const char EMP_FNAME[] = "employee.csv";
 const char DEPT_FNAME[] = "department.csv";
 const char OUTPUT_FNAME[] = "join.csv";
 const char LOG_FNAME[] = "joinLog";
+const bool TEST_CRASH = true;
+const int STABILITY = 35;
 
 // Main function
 int main(){
     FILE* depFile = openFile(DEPT_FNAME, "r");
     FILE* empFile = openFile(EMP_FNAME, "r");
-    FILE* outFile = openFile(OUTPUT_FNAME, "w");
-    join(outFile, depFile, empFile);
-    fclose(depFile);
-    fclose(empFile);
-    fclose(outFile);
-    exit(0);
+    FILE* outFile;
+    FILE* logFile;
+    if(fileExits(LOG_FNAME) && fileExits(OUTPUT_FNAME)){
+        
+        outFile = fopen(OUTPUT_FNAME, "a");
+        recover(logFile, depFile, empFile, outFile);
+    } else {
+        outFile = openFile(OUTPUT_FNAME, "w");
+        logFile = openFile(LOG_FNAME, "w"); 
+        join(outFile, depFile, empFile, logFile);
+        fclose(depFile);
+        fclose(empFile);
+        fclose(outFile);
+        exit(0);
+    }
 }
+
+//checks if a file exits and is non empty.
+//takes a file name and returns weather or not that file exists and is nonempty.
+bool fileExits(const char* fname) {
+    bool result;
+    FILE* fp = fopen(fname, "r");
+    result = (fp != NULL);
+    if(fp != NULL) {
+        result = fgetc(fp) != EOF;
+    }
+    return result;
+}
+
+
 
 //this function preforms the join by reading in one tuple from each file
 //and attempting the join. IN the future this function will also be responsible for
 //preforming actions to log all steps.
-void join(FILE* output, FILE* depFile, FILE* empFile){
+void join(FILE* output, FILE* depFile, FILE* empFile, FILE* logFile){
     //set up variables to hold read in values
     employee* emp;
     department* dep;
@@ -119,6 +160,9 @@ void join(FILE* output, FILE* depFile, FILE* empFile){
     emp = getEmpTouple(empFile);
     dep = getDeptTouple(depFile);
     bool isDone = emp == NULL || dep == NULL;
+
+    //will replace these later with the positions of the files when we start.
+    outPutLog(logFile, ftell(depFile), ftell(empFile), ftell(output));
     do {
         displayDep(dep);
         displayEmp(emp);
@@ -128,19 +172,24 @@ void join(FILE* output, FILE* depFile, FILE* empFile){
                 cout << "FOUND A MATCH\n";
                 empDepartment* toOutput = copy(dep, emp);
                 writeEmpDepartment(output, toOutput);
+                outPutLog(logFile, ftell(depFile), ftell(empFile), ftell(output));
                 delete toOutput;
                 delete dep;
                 dep = getDeptTouple(depFile);
+                outPutLog(logFile, ftell(depFile), ftell(empFile), ftell(output));
+
             //if emp is > dep discard of dep and read in a new emp
             } else if(emp->eid > dep->managerId){
                 cout << "EMP > DEP \n";
                 delete dep;
                 dep = getDeptTouple(depFile);
+                outPutLog(logFile, ftell(depFile), ftell(empFile), ftell(output));
              //if dep > emp discard emp and read in a new emp.
             } else {
                 cout << "DEP > EMP\n";
                 delete emp;
                 emp = getEmpTouple(empFile);
+                outPutLog(logFile, ftell(depFile), ftell(empFile), ftell(output));
             }
         }
         //if either emp or dep are set to null we have hit EOF
@@ -148,34 +197,39 @@ void join(FILE* output, FILE* depFile, FILE* empFile){
     } while(!isDone);
 }
 
-//reads in and sorts the DEP file then writes the result to the file. made this so
-//I could sort the files i was given in the given format. RUN THIS FUNCTION TO SORT THE
-//department.csv file so that it is in accending order on mangerid.
-void sortDep(){
-    FILE* depFile = openFile(DEPT_FNAME, "r");
-    department** toSort = new department*[100];
-    for(int j = 0; j < 100; j++){
-        toSort[j] = NULL;
+//this function takes a FILE pointer and long ints for the position of the department file,
+//employee file and the join file, creates a logEntry object and outputs to the log file.
+void outPutLog(FILE* logFile, long int dep, long int emp, long int output){
+    if(!logFile){
+        return;
     }
-    department* toAdd;
-    toAdd = getDeptTouple(depFile);
-    int i = 0;
-    while(toAdd != NULL)
-    {
-        toSort[i] = toAdd;
-        i++;
-        toAdd = getDeptTouple(depFile);
-        displayDep(toAdd);
-    }
-    std::sort(toSort, toSort + i, compareDep);
-    fclose(depFile);
-    depFile = openFile(DEPT_FNAME, "w");
-    for(int j = 0; j < i; j++){
-        writeDepartment(depFile ,toSort[j]);
-    }
-    fclose(depFile);
+    logEntry entry;
+    entry.depPos = dep;
+    entry.empPos = emp;
+    entry.joinPos = output;
+    writeLog(logFile, &entry);
 }
 
+//this file will read the log file and determine what position the 
+//depFile and empFile need to be set to. It will then set those files at
+//the last known position in the log file.
+void recover(FILE* logFile, FILE* depFile, FILE* empFile){
+    logEntry* state = getLastEntry(logFile);
+}
+
+
+//reads in the last entry from the logfile.
+logEntry* getLastEntry(FILE* logFile){
+
+}
+
+//this function will randomly crash our program. This is used to check that our program is indeed
+//crash resistant.
+void testCrash(){
+    if(TEST_CRASH && rand() % STABILITY == 0){
+        abort();
+    }
+}
 
 //this function takes a file name and a string of args
 //opens up the file and stops the program if the file
@@ -303,3 +357,29 @@ void displayDepEmp(empDepartment* ed){
 	}
 }
 
+// This function takes a logEntry pointer and a File pointer and writes the log
+// entry to the log file.
+void writeLog(FILE* log, logEntry* entry){
+    if(!log){
+        return;
+    } else {
+        fprintf(log, "%i, %i, %i\n", entry->depPos, entry->empPos, entry->joinPos);
+    }
+}
+
+//this function takes a FILE* and reads a log entry from that FILE* and
+//returns it.
+logEntry* readLog(FILE* log){
+    logEntry* entry = new logEntry;
+    if(!feof(log)) {
+        fscanf(log, "%i, %i, %i\n", &entry->depPos, &entry->empPos, &entry->joinPos);
+    } else {
+        entry = NULL;
+    }
+}
+
+// This function displays a log entry out to the console.
+// This is used for debugging only.
+void displayLog(logEntry* toDisplay){
+    cout << "DepPos: " << toDisplay->depPos << " EmpPos: " << toDisplay << " joinPos: " << toDisplay->joinPos << "\n";
+}
